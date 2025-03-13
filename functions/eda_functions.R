8f0ab84099388a84d14d7e80cd8d7db983799b31
Ben Duffin <benjamin.duffin@noaa.gov>
2025-03-12 23:31:38 -0400

    Update to old project

--- functions/eda_functions.R
+++ functions/eda_functions.R
@@ -20,30 +20,4 @@ length_unique <- function(...) {
   lapply(dat_list, function(x) length(unique(x)))
 }
 
-# function that gives number of NA and perc of data of NA
-sumNA <- function(...) {
-  # data frame logic
-  if (is.data.frame(...)) {
-    x <- data.frame(colSums(is.na(...)))
-    names(x) <- "n_NA"
-    l <- nrow(...)
-    x$perc_NA <- round(x$n_NA / l, 2)
-    print(x)
-    
-    ###################################################################### - need to work on the second part 
-  } else { # multiple vectors 
-    message("Input must be a data frame")
-    #   #vec_list <- tibble::lst(dlr$SAFIS_DEALER_RPT_ID, dlr$DEALER_TICKET_NO)
-    # vec_list <- list(...)
-    # res <- lapply(vec_list, function(x) {
-    #   sum_na <- sum(is.na(x))
-    #   tot_recs <- length(x)
-    #   perc_na <- round(sum_na / tot_recs, 2)
-    #   na_df <- data.frame(t(c(sum_na, perc_na)))
-    #   #names(na_df) <- c("n_NA", "perc_NA")
-    # })
-    
-    #bind_rows(res) 
-  }
-}  
 
